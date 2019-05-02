9b388cf80578456eaeb4a499afcdfbd4e350963c
Mathias Givskov <mgi@mcb.dk>
2019-05-02 09:20:03 +0200

    fixed jqueru selector for production.mcb.dk

--- src/scripts/content/McbProduction.js
+++ src/scripts/content/McbProduction.js
@@ -9,7 +9,7 @@ togglbutton.render('td.cell-buttons:not(.toggl)', { observe: true }, function (e
   const path = window.location.pathname;
   if (path.indexOf('support.asp') > 0) {
     projectName = 'support';
-    tags.push($('#projectAccount option:selected').innerHTML);
+    tags.push($('#projectAccount option:checked').innerHTML);
   } else {
     projectName = $('#breadcrumbs > a:last-child').innerHTML;
     tags.push($('#consultant > div.content > div.row-fluid.entity > div > div:nth-child(4) > div.controls.hasText').innerHTML);
